6f4a7f95b5e25650d3ff287939cba50cb7b37b10
Enric Florit <efz1005@gmail.com>
2025-10-10 16:42:14 +0200

    Start with conductor sieving

--- sketch.m
+++ sketch.m
@@ -1,5 +1,8 @@
+load "Demo2.magma";
+
 
 function CharactersOfOrder(G, n)
+    /* Todo: modulo inverses */
     Zn := AdditiveGroup(Integers(n));
     GZn, t := Hom(G, Zn);
     return {t(f) : f in GZn | Order(f) eq n};
@@ -16,12 +19,42 @@ function CharacterFactorsThrough(A, B, pi, chi)
     return true;
 end function;
 
-A := UnitGroup(Integers(36));
-B := UnitGroup(Integers(12));
-AB, t := Hom(A,B);
-pi := t(AB.1);
-for chi in CharactersOfOrder(A, 2) do
-   if CharacterFactorsThrough(A, B, pi, chi) then
-     print(chi);
-   end if;
+// An example: find the conductor of the characters of order 4
+
+F := UnramifiedExtension(pAdicField(2,100),2);
+K := FieldOfFractions(AllExtensions(F,2)[1]);
+f := 6; // This will change according to the situation
+Groups, Maps, Lift := ConComplex(K,f);
+// Lift(Groups[1].1);
+// Maps[4](Maps[3](Maps[2](Maps[1](Groups[1].2))));
+// Maps[2](Groups[2].1);
+
+G := Groups[1];
+gens := Generators(G);
+G;gens;
+AllChars := CharactersOfOrder(G, 4);
+
+for chi in AllChars do
+    chi_cond := f;
+    G := Groups[1];
+    while chi_cond gt 3 and  \
+        CharacterFactorsThrough(G, Groups[f - chi_cond + 2], Maps[f - chi_cond + 1], chi) do
+        chi_cond := chi_cond - 1;
+    end while;
+    print([chi(g) : g in gens]);
+    print(chi_cond);
+    print("");
 end for;
+
+/* ********************** */
+
+// Old test:
+// A := UnitGroup(Integers(36));
+// B := UnitGroup(Integers(12));
+// AB, t := Hom(A,B);
+// pi := t(AB.1);
+// for chi in CharactersOfOrder(A, 2) do
+//    if CharacterFactorsThrough(A, B, pi, chi) then
+//      print(chi);
+//    end if;
+// end for;
