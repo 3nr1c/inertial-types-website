7fdfcf515cd6a18f29faa2b24b9c274f665c86c0
Enric Florit <efz1005@gmail.com>
2025-10-20 13:06:34 +0200

    Initial code for exceptionals. Functions in Intertial types are unstable

--- inertial-types3.m
+++ inertial-types3.m
@@ -82,6 +82,55 @@ function SupercuspidalRamified3(F, K, f, c, VarepsGenerators)
         func< chi, lift | VarepsilonFilter(VarepsGenerators, 3, chi, lift)>);
 end function;
 
+function SupercuspidalRamified2args(F, K)
+    p, ram_deg, in_deg, pi, N := BaseValues(F);
+    Cond, pi, Gal, y := ExtValues(F,K);
+    f := Max(N-Cond, 2*Cond);
+    c := Cond;
+
+    UGroups, UMaps, ULift := UComplex(F, f);
+    G := UGroups[f - c + 1];
+    if f - c + 1 eq 1 then
+        llift := ULift;
+    else
+        llift := Inverse(UMaps[f - c]) * ULift;
+    end if;
+    VarepsGenerators := {K!llift(g) : g in Generators(G)};
+
+    if p eq 2 then
+        return SupercuspidalRamified2(F, K, f, c, VarepsGenerators);
+    elif p eq 3 then
+        return SupercuspidalRamified3(F, K, f, c, VarepsGenerators);
+    else
+        error Error("Prime must be 2 or 3");
+    end if;
+end function;
+
+function SupercuspidalRamified2withoutArgs(F, K)
+    assert Prime(F) eq 2;
+
+    f, c, VarepsGenerators := GetVarepsilonGenerators(F, K);
+    Cond, pi, Gal, y := ExtValues(F,K);
+
+    if (in_deg mod 2) eq 0 then
+        // (in_deg mod 2) eq 0 checks if x^2+x+1 splits in F
+        // Triply imprimitive with n = 4, characters must be quadratic on y
+        quadratic_filter := func< chi, lift | IsIdentity(2 * chi(Inverse(lift)(y)))>;
+    else
+        // Simply imprimitive with n = 4, characters must *not* be quadratic on y
+        quadratic_filter := func< chi, lift | not IsIdentity(2 * chi(Inverse(lift)(y)))>;
+    end if;
+    CGroups, CMaps, CLift := ConComplex(F, K, f);
+    // CGroups[1];
+    return ComputeChars(4, func< | CGroups, CMaps, CLift >, 
+                [
+                    quadratic_filter,
+                    func< chi, lift | VarepsilonFilter(VarepsGenerators, 2, chi, lift)>
+                ]), 
+        CGroups, CMaps, CLift;
+end function;
+
+
 function SupercuspidalRamified(F, K, f, c, VarepsGenerators)
     p := Prime(F);
     if p eq 2 then
@@ -99,29 +148,28 @@ function InertialTypes(F)
 
     p, ram_deg, in_deg, pi, N := BaseValues(F);
     ff := Floor(N/2);
-    print("f=");print(ff);
+    printf "f=%o\n", ff;
     groups, maps, lift := UComplex(F, ff);
     #groups;
 
     chars := PrincipalSeries(F, ff : MyComplex := [*groups, maps, lift*]);
     #chars;
-    [[*char[2],char[3]*] : char in chars];
+    // [[*char[2],char[3]*] : char in chars];
     print("----");
 
     i := 1;
     for K in QuadExt do
+        i;
         Cond, pi, Gal, y := ExtValues(F,K);
         c := Cond;
-        // print(f);print(c);
         if Cond eq 0 then
             chars := SupercuspidalUnramified(F,K,ff);
             #chars;
-            [[*char[2],char[3]*] : char in chars];
+            // [[*char[2],char[3]*] : char in chars];
         else 
-            i;
             f := Max(N-Cond, 2*Cond);
-            print("f=");print(f);
-            print("c=");print(c);
+            printf "f=%o\n", f;
+            printf "c=%o\n", c;
             G := groups[ff - c + 1];
             if ff - c + 1 eq 1 then
                 llift := lift;
@@ -129,10 +177,9 @@ function InertialTypes(F)
                 llift := Inverse(maps[ff - c]) * lift;
             end if;
             VarepsGenerators := {K!llift(g) : g in Generators(G)};
-
             chars := SupercuspidalRamified(F, K, f, c, VarepsGenerators);
-            #chars;
-            [[*char[2],char[3]*] : char in chars];
+            printf "%o characters\n", #chars;
+            // [[*char[2],char[3]*] : char in chars];
             print("----");
         end if;
         i +:= 1;
@@ -143,3 +190,4 @@ end function;
 
 Q2 := pAdicField(2,100);
 Q4 := UnramifiedExtension(Q2, 2);
+K := FieldOfFractions(AllExtensions(Q2, 2)[1]);
